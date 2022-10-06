ba1591ed14e3aa6c87033f6cc99b24b021f1770b
zun010 <mailforzun@gmail.com>
2022-10-06 19:17:00 +0300

    Initial commit

---
+++ ConsoleApplication/ConsoleApplication/Tests/CalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleApplication
+{
+    public sealed class CalculatorTests
+    {
+        [Test]
+        public void TestSquareEquation()
+        {
+            var calculator = new Calculator();
+
+            var actualX1 = calculator.SquareEquation(3, 7, -10);
+            var expectedX1 = 1;
+
+            Assert.AreEqual(expectedX1, actualX1);
+        }
+    }
+}
